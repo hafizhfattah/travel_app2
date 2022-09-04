319d940e50985bb2449f9cfe888bfb9092579ef6
fizfat <hafizhfattah99@gmail.com>
2022-09-04 14:43:56 +0700

    second push

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: AuthService().handleAuthState(),
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
